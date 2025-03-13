57936c1d7fcf54a312cf226e38af30d0b128e115
Yesmine <yesmine.hachana@dauphine.eu>
2025-03-13 16:05:07 +0100

    First commit to check if github works :)

---
+++ Macro_Project.m
@@ -0,0 +1,7 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%
+% MACROECONOMICS PROJECT %
+%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+cd '/Users/yesminehachana/Desktop/Classes/Dauphine/2nd Semester/Macroeconomics MATLAB Project/Repository'
+
